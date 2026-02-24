6febf989dd257de121aedf24df68fe7b8a416905
rainathecreatour-ops <rainathecreatour@gmail.com>
2026-02-24 04:58:18 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
   root: '.',
   build: {
     outDir: 'dist',
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-    },
   },
 })
